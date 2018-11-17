efe396c9f3115462b6c942d878113c241f1347cf
treken <kevinchupe@icloud.com>
2018-11-17 13:45:41 -0600

    Load movie posters using Glide.

--- app/src/main/java/com/treken/someapp/MoviesAdapter.java
+++ app/src/main/java/com/treken/someapp/MoviesAdapter.java
@@ -4,11 +4,16 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 
 import java.util.List;
 
 public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.MovieViewHolder> {
+    private String IMAGE_BASE_URL = "http://image.tmdb.org/t/p/w500";
 
     private List<Movie> movies;
 
@@ -36,21 +41,27 @@ public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.MovieViewH
         TextView releaseDate;
         TextView title;
         TextView rating;
-        TextView genres;
+        TextView genres; // TODO: Add genres
+        ImageView poster;
 
         public MovieViewHolder(View itemView) {
             super(itemView);
             releaseDate = itemView.findViewById(R.id.item_movie_release_date);
             title = itemView.findViewById(R.id.item_movie_title);
             rating = itemView.findViewById(R.id.item_movie_rating);
-            genres = itemView.findViewById(R.id.item_movie_genre);
+            genres = itemView.findViewById(R.id.item_movie_genre); // Add genres later
+            poster = itemView.findViewById( R.id.item_movie_poster );
         }
 
         public void bind(Movie movie) {
             releaseDate.setText(movie.getReleaseDate().split("-")[0]);
             title.setText(movie.getTitle());
             rating.setText(String.valueOf(movie.getRating()));
-            genres.setText("");
+            genres.setText(""); // Add genres later
+            Glide.with(itemView)
+                    .load( IMAGE_BASE_URL + movie.getPosterPath() )
+                    .apply( RequestOptions.placeholderOf( R.color.colorPrimary ) )
+                    .into( poster );
         }
     }
 }
