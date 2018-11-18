abf851de7f82717309954cf602d1afe8829798dd
treken <kevinchupe@icloud.com>
2018-11-18 08:42:37 -0600

    Add toolbar and menu. Refactor layout by creating a layout for movies list. Adjust layout so toolbar remains visible by using padding.

--- app/src/main/java/com/treken/someapp/MainActivity.java
+++ app/src/main/java/com/treken/someapp/MainActivity.java
@@ -1,9 +1,11 @@
 package com.treken.someapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.widget.Toast;
 
 import java.util.List;
@@ -19,6 +21,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         moviesRepository = MoviesRepository.getInstance();
 
@@ -37,5 +42,11 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, "Please check your internet connection.", Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
